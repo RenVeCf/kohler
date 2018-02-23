de30663b4645873c51e212e06dc485ab26b96f20
renmengyang <942685687@qq.com>
2018-02-23 17:53:38 +0800

    UI

--- app/src/main/java/com/mengyang/kohler/home/activity/BarrageActivity.java
+++ app/src/main/java/com/mengyang/kohler/home/activity/BarrageActivity.java
@@ -1,30 +1,16 @@
 package com.mengyang.kohler.home.activity;
 
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
 import com.gyf.barlibrary.ImmersionBar;
 import com.mengyang.kohler.App;
 import com.mengyang.kohler.BaseActivity;
 import com.mengyang.kohler.R;
 import com.mengyang.kohler.common.net.Config;
-import com.mengyang.kohler.common.net.IConstants;
-import com.mengyang.kohler.common.utils.LogUtils;
-import com.mengyang.kohler.common.utils.SPUtil;
-import com.mengyang.kohler.common.utils.ToastUtil;
-import com.mengyang.kohler.common.view.SpacesItemDecoration;
 import com.mengyang.kohler.common.view.TopView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 
 import butterknife.BindView;
 import okhttp3.Call;
@@ -35,11 +21,13 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+/**
+ * 我要爬墙
+ */
+
 public class BarrageActivity extends BaseActivity {
     @BindView(R.id.tv_barrage_top)
     TopView tvBarrageTop;
-    @BindView(R.id.rv_barrage)
-    RecyclerView rvBarrage;
 
     @Override
     protected int getLayoutId() {
@@ -51,16 +39,6 @@ public class BarrageActivity extends BaseActivity {
         App.getManager().addActivity(this);
         //防止状态栏和标题重叠
         ImmersionBar.setTitleBar(this, tvBarrageTop);
-        // 设置管理器
-        LinearLayoutManager layoutManager = new LinearLayoutManager(App.getContext());
-        rvBarrage.setLayoutManager(layoutManager);
-        rvBarrage.addItemDecoration(new SpacesItemDecoration(20));
-        // 如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
-        rvBarrage.setHasFixedSize(true);
-        rvBarrage.setItemAnimator(new DefaultItemAnimator());
-        //        mSystemMsgBean = new ArrayList<>();
-        //        mSystemMsgAdapter = new SystemMsgAdapter(mSystemMsgBean);
-        //        rvBarrage.setAdapter(mSystemMsgAdapter);
     }
 
     @Override
@@ -70,6 +48,10 @@ public class BarrageActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+
+    }
+
+    private void SendBarrage() {
         new Thread() {
             @Override
             public void run() {
