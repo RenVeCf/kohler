99179f695c6fb6957fe0806e8f1e45126453d31c
18516106381 <18516106381@163.com>
2018-03-02 22:14:22 +0800

    log_修改我的手册界面。

---
+++ app/src/main/java/com/mengyang/kohler/common/utils/FileUtil.java
@@ -0,0 +1,40 @@
+package com.mengyang.kohler.common.utils;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by liusong on 2018/3/2.
+ */
+
+public class FileUtil {
+
+    /**
+     * 查看本地是否有PDF文件
+     */
+    public static List<String> judgePdfIsExit(List<String> localTempPdfFileName) {
+
+        File file2 = new File(Contants.mRootPath);
+
+        if (!file2.exists()) {
+            file2.mkdirs();
+        }
+
+        File[] files = file2.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            String fileName = files[i].getName();
+
+            if (files[i].isFile() && fileName.endsWith(".pdf")) {
+                if (!TextUtils.isEmpty(fileName)) {
+                    localTempPdfFileName.add(fileName);
+                }
+
+                Log.i("456789","pdfName = " + fileName);
+            }
+        }
+        return localTempPdfFileName;
+    }
+}
